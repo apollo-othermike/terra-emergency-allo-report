c82428b74ac35144f01e02a134a1b6b81a2b19ae
othermike <othermike@apollo.farm>
2022-12-21 17:06:10 -0500

    Updates

---
+++ _next/static/2Ail2E8kIMfCb-3zHIxQI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/518-a5d3b0dda8cfd66f.js","static/chunks/pages/index-a72638c4deb1e6b3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
